c0a7b5b51d050e37c30a8a8c660a0ef91a980a51
ujdemon777 <145430273+ujdemon777@users.noreply.github.com>
2023-10-11 18:40:04 +0530

    Delete forms.py

--- forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class StudentForm(forms.Form):
-    firstname = forms.CharField(label="Enter first name", max_length=50)
-    file = forms.FileField()  # for creating file input
